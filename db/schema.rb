f38105d4fb694e241239d83faf515d4ba399133f
Michael-Mehta <supermichael2001@gmail.com>
2022-10-26 11:03:37 -0700

    Third commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,12 @@
 ActiveRecord::Schema[7.0].define(version: 2022_10_26_013201) do
   create_table "comments", force: :cascade do |t|
     t.string "description"
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
